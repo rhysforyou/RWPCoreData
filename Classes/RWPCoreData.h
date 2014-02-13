0a1a9693b8aca9c67da669ac26c3998130d4a3a0
Rhys Powell <rhys@rpowell.me>
2014-02-13 18:57:12 +1100

    Added RWPCoreData.h

---
+++ Classes/RWPCoreData.h
@@ -0,0 +1,11 @@
+//
+//  RWPCoreData.h
+//  RWPCoreData
+//
+//  Created by Rhys Powell on 12/01/2014.
+//  Copyright (c) 2014 Rhys Powell. All rights reserved.
+//
+
+#import "RWPManagedObject.h"
+#import "RWPRemoteManagedObject.h"
+#import "NSDictionary+RWPExtensions.h"
